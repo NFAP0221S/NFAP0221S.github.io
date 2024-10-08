048679cbfc30ae32337e81c14b667dc24a51e0b1
Hansol <102638663+NFAP0221S@users.noreply.github.com>
2024-10-08 23:58:46 +0900

    shadcn 교체중 ...

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,4 @@
-import type { Config } from "tailwindcss"
-import {nextui} from "@nextui-org/react";
-
+/** @type {import('tailwindcss').Config} */
 const config = {
   darkMode: ["class"],
   content: [
@@ -31,25 +29,8 @@ const config = {
     },
   },
   plugins: [
-    nextui({
-      prefix: "nextui", // prefix for themes variables
-      addCommonColors: false, // override common colors (e.g. "blue", "green", "pink").
-      defaultTheme: "light", // default theme from the themes object
-      defaultExtendTheme: "light", // default theme to extend on custom themes
-      layout: {}, // common layout tokens (applied to all themes)
-      themes: {
-        light: {
-          layout: {}, // light theme layout tokens
-          colors: {}, // light theme colors
-        },
-        dark: {
-          layout: {}, // dark theme layout tokens
-          colors: {}, // dark theme colors
-        },
-        // ... custom themes
-      },
-    }),
+    
   ],
-} satisfies Config
+}
 
 export default config
