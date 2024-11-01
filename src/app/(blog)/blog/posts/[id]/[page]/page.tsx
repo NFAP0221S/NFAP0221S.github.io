2017cc66242cf3f99f2572ed6b17269ae4305e71
Hansol <102638663+NFAP0221S@users.noreply.github.com>
2024-11-01 17:18:58 +0900

    posts 서버 데이터 병렬로 패칭 처리

--- src/app/(blog)/blog/posts/[id]/[page]/page.tsx
+++ src/app/(blog)/blog/posts/[id]/[page]/page.tsx
@@ -1,29 +1,58 @@
-import { notFound } from 'next/navigation';
-import React from 'react';
-import { PostsPage } from '@/pages/posts-page';
-import { getDatabase, getBlocks, getPage } from '@/shared/lib/notion';
+import { notFound } from "next/navigation";
+import { PostsPage } from "@/pages/posts-page";
+import { getDatabase, getBlocks, getPage } from "@/shared/lib/notion";
 
 // generateStaticParams 함수 추가
+// export const generateStaticParams = async () => {
+//   const databaseId = process.env.NEXT_PUBLIC_NOTION_DATABASE_ID as string;
+//   if (!databaseId) {
+//     throw new Error("NEXT_PUBLIC_NOTION_DATABASE_ID is not defined");
+//   }
+//   const posts = await getDatabase(databaseId);
+//   // console.log('@@@ databaseId', databaseId)
+//   // console.log('@@@ posts', posts.length)
+//   const paramsArray = [];
+//   for (const post of posts) {
+//     const blocks = await getBlocks(post.id);
+//     // console.log('@@@ blocks', blocks)
+//     const totalPages = Math.ceil(blocks.length / 5);
+//     for (let page = 1; page <= totalPages; page++) {
+//       paramsArray.push({ id: post.id, page: page.toString() });
+//     }
+//     // console.log('@@@ paramsArray', paramsArray)
+//   }
+//   console.log('### 서버 데이터 가져오는 중 ###')
+//   return paramsArray;
+// };
+
 export const generateStaticParams = async () => {
   const databaseId = process.env.NEXT_PUBLIC_NOTION_DATABASE_ID as string;
   if (!databaseId) {
     throw new Error("NEXT_PUBLIC_NOTION_DATABASE_ID is not defined");
   }
+
   const posts = await getDatabase(databaseId);
-  console.log('@@@ databaseId', databaseId)
-  console.log('@@@ posts', posts.length)
-  const paramsArray = [];
-  for (const post of posts) {
+  const paramsArray: { id: string; page: string; }[] = [];
+
+  // 모든 posts의 getBlocks 호출을 병렬 처리
+  const blockPromises = posts.map(async (post) => {
     const blocks = await getBlocks(post.id);
-    console.log('@@@ blocks', blocks)
     const totalPages = Math.ceil(blocks.length / 5);
-    for (let page = 1; page <= totalPages; page++) {
-      paramsArray.push({ id: post.id, page: page.toString() });
-    }
-    console.log('@@@ paramsArray', paramsArray)
-  }
+    const postPages = Array.from({ length: totalPages }, (_, i) => ({
+      id: post.id,
+      page: (i + 1).toString(),
+    }));
+    return postPages;
+  });
+
+  // 모든 블록 요청이 완료될 때까지 대기하고 결과 병합
+  const results = await Promise.all(blockPromises);
+  results.forEach((postPages) => paramsArray.push(...postPages));
+
+  console.log("### 서버 데이터 가져오는 중 ###");
   return paramsArray;
-}
+};
+
 
 interface props {
   params: {
@@ -35,7 +64,7 @@ interface props {
 const Posts = async ({ params }: props) => {
   const { id, page } = params;
   const currentPage = parseInt(page, 10);
-  
+
   const pageData: any = await getPage(id);
   const blocks: any = await getBlocks(id);
   const postsProps = { id, initialBlocks: blocks, currentPage };
@@ -44,8 +73,7 @@ const Posts = async ({ params }: props) => {
     notFound();
   }
 
+  return <PostsPage {...postsProps} />;
+};
 
-  return <PostsPage {...postsProps}/>;
-}
-
-export default Posts
+export default Posts;
